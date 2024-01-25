70cd10c7776f5d7233feb1839d04fe5361c4940e
Rakhman <231355@astanait.edu.kz>
2024-01-25 19:04:55 +0600

    1.1

---
+++ src/Person.java
@@ -0,0 +1,33 @@
+public class Person {
+    private static int nextId = 1;
+    private int id;
+    private String name;
+    private String surename;
+    public Person(){
+        this.id = nextId++;
+    }
+    public Person(String surname, String name){
+        this();
+        this.name = name;
+        this.surename = surname;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName (String name){
+        this.name=name;
+    }
+    public String getSurename(){
+        return surename;
+    }
+    public void setSurename(String surename){
+        this.surename = surename;
+    }
+    @Override
+    public String toString(){
+        return id +") "+surename+" "+name;
+    }
+}
